02b1b7d758c5650f23ffbdf7f93ba8edd8745b05
danila-pastukhov <danila.pastukhov@gmail.com>
2026-02-06 07:23:09 +0300

    fix label
    "Вступил в группы FIXLATER TODO"

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -116,7 +116,7 @@ const ProfilePage = ({ authUsername }) => {
       {joinedGroups.length > 0 && (
         <div className="max-container padding-y">
           <h2 className="text-2xl font-semibold mb-6 text-[#181A2A] dark:text-white">
-            Вступил в группы FIXLATER TODO
+            Вступил в группы
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {joinedGroups.map((group) => (
