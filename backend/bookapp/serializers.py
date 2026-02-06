dd1713058f83bbdf4824c653adf41ecc40274bf6
danila-pastukhov <danila.pastukhov@gmail.com>
2026-02-06 15:00:54 +0300

    Revert "fix n+1 reqs in count of comments"
    This reverts commit eea1e29b455bec7913d1fecb87e99f756c99c420.

--- backend/bookapp/serializers.py
+++ backend/bookapp/serializers.py
@@ -416,6 +416,7 @@ class UserToReadingGroupStateSerializer(serializers.ModelSerializer):
 
 
 class BookCommentSerializer(serializers.ModelSerializer):
+    """Serializer for book comments with user and book info."""
 
     user = SimpleAuthorSerializer(read_only=True)
     book_slug = serializers.CharField(source="book.slug", read_only=True)
@@ -426,7 +427,6 @@ class BookCommentSerializer(serializers.ModelSerializer):
     reading_group_name = serializers.CharField(
         source="reading_group.name", read_only=True, allow_null=True
     )
-    # Must be annotated in queryset: .annotate(replies_count=Count("replies"))
     replies_count = serializers.IntegerField(read_only=True)
     is_reply = serializers.BooleanField(read_only=True)
 
