062d1c8e657c1491ac67eee8278f4382761f5ec8
danila-pastukhov <danila.pastukhov@gmail.com>
2026-02-06 16:32:05 +0300

    Reapply "fix race conditions in finishing group tasks"
    This reverts commit 837debfb8bc431b4b9447ac99bc98ff7cb024d34.

--- backend/bookapp/serializers.py
+++ backend/bookapp/serializers.py
@@ -73,7 +73,6 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         username = validated_data["username"]
         first_name = validated_data["first_name"]
         last_name = validated_data["last_name"]
-        first_name = validated_data["first_name"]
         password = validated_data["password"]
 
         user = get_user_model()
